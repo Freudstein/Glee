b8743103c324ee3211d0e8438ecc8d5a30fae251
Freudstein <dark.necropolis@gmail.com>
2023-05-29 00:57:56 +0300

    Added Top slider, Offer, Products sections

--- src/js/app.js
+++ src/js/app.js
@@ -7,3 +7,37 @@ menuBtn.addEventListener('click', function(){
     menu.classList.toggle('active');
     menuBtn.classList.toggle('active');
 })
+
+// top-slider
+
+const topSlider = new Swiper('.top-slider', {
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return `<span class="${className}">${index + 1}</span>`;
+          },              
+    },
+
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    loop: true,
+});
+
+// products of the week - filtering cards
+
+const mixer = mixitup('.products__cards');
+
+const filterMenu = document.querySelector('.products__menu');
+const menuLinks = document.querySelectorAll('.products__link');
+filterMenu.addEventListener('click', function(evt){
+    let target = evt.target;
+    if(target.classList.contains('products__link')){
+        menuLinks.forEach( elem => {
+            elem.classList.remove('products__link--current');
+        })
+        target.classList.add('products__link--current');
+    }
+})
